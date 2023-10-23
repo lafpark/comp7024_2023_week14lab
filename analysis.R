9704913c8ce4d2e37c1f08883f87c98edef12b88
Laurence Park <lapark@scem.uws.edu.au>
2023-10-23 11:54:24 +1100

    Made x axis show month and year.

--- analysis.R
+++ analysis.R
@@ -1,9 +1,12 @@
 
 d <- read.csv("epiSEIHCRD_combAge.csv")
 d$t <- as.Date(d$t, origin = "2020-03-01")
+d$t <- as.POSIXct(d$t)
 
 library("ggplot2")
+library("scales")
 
 ggplot(data = d) +
-  geom_line(mapping = aes(x = t, y = H))
+  geom_line(mapping = aes(x = t, y = H)) +
+  scale_x_datetime(labels = date_format("%b %Y"))
 
