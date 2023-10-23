3dbeaf971e78c477727f63ccc3f440de388fe555
Laurence Park <lapark@scem.uws.edu.au>
2023-10-23 12:25:18 +1100

    Converted y scale to log.

--- analysis.R
+++ analysis.R
@@ -9,5 +9,6 @@ library("scales")
 ggplot(data = d) +
   geom_line(mapping = aes(x = t, y = H), col = "red") +
   scale_x_datetime(labels = date_format("%b %Y")) +
-  labs(x = "Date", y = "Number of hospital beds")
+  labs(x = "Date", y = "Number of hospital beds") +
+  scale_y_log10()
 
